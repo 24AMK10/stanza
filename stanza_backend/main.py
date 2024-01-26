908c3241b35e82941c9353e3da6b2c29012c5048
Ashish_Maurya <ashish.s.maurya123@gmail.com>
2024-01-26 21:57:02 +0530

    chore : main.py bug fix

--- stanza_backend/main.py
+++ stanza_backend/main.py
@@ -1,8 +1,10 @@
-from app import init_app
+from src import init_app
+from flask_cors import CORS
+
 
 app = init_app()
 
-
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5005)
